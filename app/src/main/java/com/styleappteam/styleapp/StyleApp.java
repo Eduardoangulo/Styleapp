84191a664225f30eaf7f4ea3a1f0eadd54637d48
Luis Echenique <luisechenique114@gmail.com>
2017-06-11 03:08:12 -0500

    Facebook sdk added

---
+++ app/src/main/java/com/styleappteam/styleapp/StyleApp.java
@@ -0,0 +1,19 @@
+package com.styleappteam.styleapp;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by Luis on 11/06/2017.
+ */
+
+public class StyleApp extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
