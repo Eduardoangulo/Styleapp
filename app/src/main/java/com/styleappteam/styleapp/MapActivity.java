0d7c4014356af50ce02042a7c21697b6f71766ed
Eduardo Angulo Luna <eduardoanguloluna@gmail.com>
2017-06-11 18:17:19 -0500

    API KEY enable

--- app/src/main/java/com/styleappteam/styleapp/MapActivity.java
+++ app/src/main/java/com/styleappteam/styleapp/MapActivity.java
@@ -120,7 +120,7 @@ public class MapActivity extends AppCompatActivity implements  OnMapReadyCallbac
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mapa.setMyLocationEnabled(true);
-            map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener(){
+            mapa.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener(){
                 @Override
                 public boolean onMyLocationButtonClick()
                 {
