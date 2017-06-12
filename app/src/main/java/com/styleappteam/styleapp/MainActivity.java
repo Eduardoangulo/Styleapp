42d895713fb880edf28868b5d2f53f9fc571cc72
Luis Echenique <luisechenique114@gmail.com>
2017-06-12 12:24:52 -0500

    Email Facebook, Fixed animations, Report Problem

--- app/src/main/java/com/styleappteam/styleapp/MainActivity.java
+++ app/src/main/java/com/styleappteam/styleapp/MainActivity.java
@@ -121,8 +121,8 @@ public class MainActivity extends AppCompatActivity {
     private void sendReport(){
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, R.string.StyleAppEmail);
-        intent.putExtra(Intent.EXTRA_SUBJECT, R.string.reportSubjet);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getResources().getString(R.string.StyleAppEmail)});
+        intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.reportSubjet));
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
@@ -130,8 +130,9 @@ public class MainActivity extends AppCompatActivity {
     //regresar al login despues de cerrar sesion o si no habias iniciado sesion.
     private void goLoginScreen() {
         Intent intent= new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION|Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        overridePendingTransition(0,0);
     }
 
     /*
